8e26e296fa7d339d8dfaac07f79302f8080a1d1c
Vineeth Viswan <vineeth.viswan@live.com>
2025-02-02 23:39:34 -0400

    Setting up Project

---
+++ RTXAppln/src/Main.cpp
@@ -0,0 +1,16 @@
+
+/* ---------------------------------------------------------------------------------
+ * RAY TRACING
+ * Started  : 02-Feb-2025
+ * Finished : 
+ * References : https://raytracing.github.io/books/RayTracingInOneWeekend.html
+ * ---------------------------------------------------------------------------------*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "RAY TRACING !!!\n";
+
+	return 0;
+}
